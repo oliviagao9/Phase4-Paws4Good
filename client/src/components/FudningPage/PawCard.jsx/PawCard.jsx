db1d7812eb93cd7329fdcd9f41f6e1361ea1db8d
Oliva Gao <oliviagao9@gmail.com>
2024-03-29 19:33:22 -0500

    completed donation post action on client

--- client/src/components/FudningPage/PawCard.jsx/PawCard.jsx
+++ client/src/components/FudningPage/PawCard.jsx/PawCard.jsx
@@ -4,9 +4,46 @@ import { ProgressBar } from 'react-bootstrap';
 
 const PawCard = ({petData , user}) => {
 
+  const [amount, setAmount] = useState(0);
+  const [toggle, setToggle] = useState(false);
   const {name, cause, goal, image, age,  id, donations} = petData
   const donationRaised = donations.reduce((accum,donation) => {return accum + donation.amount},0)
-  console.log(user)
+  const [newTotalFund, setNewTotalFund] = useState(donationRaised)
+
+  const handleToggle = (e) => {
+    e.preventDefault();
+    setToggle(!toggle);
+  }
+
+  const handleDonation = (e) => {
+    e.preventDefault();
+
+    const donationData = {
+        amount: amount,
+        pet_id: id,
+        donor_id: user.user_id
+    }
+
+    return fetch("/api/donations", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(donationData)
+    })
+    .then( r=> r.json())
+    .then(updatedDonation => {
+      console.log(updatedDonation);
+      console.log(newTotalFund);
+      setNewTotalFund(newTotalFund +updatedDonation.amount)
+      setAmount(0)
+      setToggle(!toggle)
+      console.log(newTotalFund);
+    }
+    )
+  }
+
   return (
     <Card>
       <ImageIcon>
@@ -16,14 +53,30 @@ const PawCard = ({petData , user}) => {
         <h4>Name: {name}</h4>
         <p><strong>Age: </strong> {age}</p>
         <p><strong>Cause: </strong> {cause}</p>
-        <p><strong>Raised: </strong> {donationRaised}</p>
+        <p><strong>Raised: </strong> {newTotalFund}</p>
         <p><strong>Target: </strong>{goal}</p>
         <ProgressBar 
           striped
           variant="info"
-          now={(donationRaised/goal) * 100} 
+          now={(newTotalFund/goal) * 100} 
           style={{ width: "50%", margin: "auto" }}
-        />    
+        />
+        {toggle?( 
+        <form on onSubmit={handleDonation}> 
+          <input             
+            type="number"
+            name="amount"
+            min="0"
+            placeholder="Amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}>
+          </input>
+        <button>Donate</button>
+        </form>) :null
+      }
+      <div className="footer">
+      {!toggle? (<button onClick = {handleToggle} className="button">Donate Now</button>):<button onClick = {handleToggle} lassName="button">Close Donation Form</button>}
+      </div>
       </CardContainer>
     </Card>
   );
