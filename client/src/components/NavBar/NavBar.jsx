c2e264c20269c3c15f7e42c02a5cc5b46f6301fd
Oliva Gao <oliviagao9@gmail.com>
2024-04-01 13:15:40 -0500

    optimized the nav bar logic betwee authenticated and not login

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -6,29 +6,39 @@ const NavBar = ( { user,  handleLogout}) => {
   return (
     <div className= "header">
       <div className= "headerLeft">
-        {!user? (<Link to ='/' style={{ color: 'black' }}>
-          <p>Paws4Good</p>
-        </Link>) : (
-        <Link to ='/fundingpage' style={{ color: 'black' }}>
-          <p>Paws4Good</p>
-        </Link>)}
+        {!user ? (
+          <Link to ='/' style={{ color: 'black' }}>
+            <p>Paws4Good</p>
+          </Link>
+          ) : (
+            <Link to ='/fundingpage' style={{ color: 'black' }}>
+              <p>Paws4Good</p>
+            </Link>
+        )}
       </div>
       <div className="headerRight">
         {!user ? (
-          null) : <div className="leftSideLink" style={{ color: 'black' }}>
-            <Link to='/addpaw'> Add Paw</Link>
-        </div>}
-        {!user ? (
-        <div className="leftSideLink" style={{ color: 'black' }}>
-          <Link to='/login'>Sign In</Link>
-        </div>) : <div className="leftSideLink" style={{ color: 'black' }}>
-          <Link to='/editprofile'> Edit Profile</Link>
-        </div>}
-        {!user ? (
-        <div className="rightSideLink" style={{ color: 'white' }}>
-          <Link to='/signup' >Get Started</Link> 
-        </div>) :  <div className="rightSideLink" Link to='/' onClick={handleLogout} > Logout </div>}
-
+          <>
+            <div className="leftSideLink" style={{ color: 'black' }}>
+              <Link to='/login'>Sign In</Link>
+            </div>
+            <div className="rightSideLink" style={{ color: 'white' }}>
+            <Link to='/signup' >Get Started</Link> 
+            </div>
+          </>
+        ):(
+          <>
+            <div className="leftSideLink" style={{ color: 'black' }}>
+              <Link to='/addpaw'> Add Paw</Link>
+            </div>
+            <div className="leftSideLink" style={{ color: 'black' }}>
+              <Link to='/editprofile'> Edit Profile</Link>
+            </div>
+            <div className="rightSideLink"> 
+              <Link to='/' onClick={handleLogout}> Logout</Link>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
