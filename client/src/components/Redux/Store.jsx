0bfe318c1e1a7a8534f417c57b9defc35d06c467
olivia <oliviagao9@gmail.com>
2024-05-11 17:47:29 -0500

    created initial store

---
+++ client/src/components/Redux/Store.jsx
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const reducer = combineReducers();
+
+const store = configureStore({
+    reducer
+});
+
+export default store;
