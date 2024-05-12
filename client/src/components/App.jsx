c6e6b364660910d64c3b7c83fc239389f559d0db
olivia <oliviagao9@gmail.com>
2024-05-12 18:47:06 -0500

    add redux to app and removed unused states

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage/LandingPage";
 import NavBar from "./NavBar/NavBar";
@@ -10,14 +10,11 @@ import { ProtectedRoute } from "./ProtectedRoute";
 import EditProfile from "./Auth/EditProfile";
 import AddPaw from "./FudningPage/AddPaw";
 import { loginSession } from "./Redux/Session";
+import { setPaw } from "./Redux/Paw";
 import { useDispatch, useSelector } from "react-redux";
 
 function App() {
 
-  const navigate = useNavigate();
-  const [userState, setUserState] = useState(null);
-  const [pets, setPets] = useState([])
-  const user = useSelector(state => state.session);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -41,7 +38,9 @@ function App() {
   const getPet = () => {
     fetch('/api/pets')
       .then(r => r.json())
-      .then(petData => setPets(petData))
+      .then(petData => {
+          dispatch(setPaw(petData));
+    });
   }
 
   return (
@@ -65,7 +64,7 @@ function App() {
           <Route
               path="/fundingpage"
               element={
-                <FundingPage pets={pets} />}
+                <FundingPage/>}
           />
           <Route
           path="/editprofile"
@@ -77,8 +76,6 @@ function App() {
           path="/addpaw"
           element={
             <AddPaw
-            pets = {pets}
-            setPet={setPets}
             />
           }
           />
