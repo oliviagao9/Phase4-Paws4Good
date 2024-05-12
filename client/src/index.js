38832e1a36ce4ee051293e024a430b7615eb6bc3
olivia <oliviagao9@gmail.com>
2024-05-12 13:52:00 -0500

    added redux provider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './components/Redux/Store';
+import { Provider } from "react-redux";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <Provider store = {store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
